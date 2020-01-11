5a0e0e445d5901aea310e8e4529b43895d31903b
KIMCOYG <kjhkjh9753@naver.com>
2020-01-12 03:01:08 +0900

    babel

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 import express from "express";
+import morgan from "morgan"; //middleware
+import helmet from "helmet"; //security
+
 const app = express();
 
 const PORT = 4000;
 
-const handleProfile = (req, res) => res.send("You are on my profile");
-
 const handleListening = () => 
     console.log(`Listening on: http://localhost:${PORT}`);
 
+const handleHome = (req, res, next) => res.send("Hello from ass");
 
-const handleHome = (req, res) => res.send("Hello from ass");
+const handleProfile = (req, res) => res.send("You are on my profile");
 
-app.get("/", handleHome);
+app.use(helmet());
+app.use(morgan("dev"));
+
+const middleware = (req, res, next) => {
+    res.send("not happening");
+}
+
+app.get("/",middleware, handleHome);
 
 app.get("/profile", handleProfile);
 
