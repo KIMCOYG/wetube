05a0c4e0fe6b117b73b46d63e13b2301b56a58bb
KIMCOYG <kjhkjh9753@naver.com>
2020-01-11 22:49:02 +0900

    ES6

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening(){
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+const handleListening = () => 
     console.log(`Listening on: http://localhost:${PORT}`);
-}
+
+
+const handleHome = (req, res) => res.send("Hello from ass");
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
