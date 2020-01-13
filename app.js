fcaf23cd79f765aaa9f4d88fba046ad4879e8b00
KIMCOYG <kjhkjh9753@naver.com>
2020-01-14 01:39:31 +0900

    First pug layouts

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localsMiddelware } from "./middlewares";
 
 const app = express();
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json()); //bodyParser은 웹사이트로 전달하는 정보들을 검사하는 middleware
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddelware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,6 +1,13 @@
 import express from "express"
+import routes from "../routes"
+import { upload, videoDetail, deleteVideo, editVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo)
 
 export default videoRouter;
 
