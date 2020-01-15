f9cc7272d001b9d527249b17852b899a26ba628e
KIMCOYG <kjhkjh9753@naver.com>
2020-01-16 02:39:57 +0900

    form

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,14 @@
-export const home = (req, res) => res.render("home", {pageTitle: "Home"}); //두 번째 arg는 template에 추가할 정보가 담긴 객체
-export const search = (req, res) => res.render("search", {pageTitle: "Search"});
+import {videos} from "../db"
+
+export const home = (req, res) => {
+    res.render("home", {pageTitle: "Home", videos});
+}; //두 번째 arg는 template에 추가할 정보가 담긴 객체
+export const search = (req, res) => {
+    const {
+        query: {term: searchingBy}
+    } = req;
+    res.render("search", {pageTitle: "Search", searchingBy});
+}
 export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"});
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Video Detail"});
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "Edit Video"});
