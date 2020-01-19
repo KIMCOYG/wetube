37b21d5df9fd0372b100daa5b2a3663d6b27d41a
KIMCOYG <kjhkjh9753@naver.com>
2020-01-19 22:39:41 +0900

    3.7 file streaming, but file not streaming i don't know this reason

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 
 app.use(helmet());
 app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads")); //directory에서 file을 보내주는 middleware
 app.use(cookieParser());
 app.use(bodyParser.json()); //bodyParser은 웹사이트로 전달하는 정보들을 검사하는 middleware
 app.use(bodyParser.urlencoded({extended:true}));
