6d9151a4fdecfd02d8bdefecc7ceb3a4b84dc49a
KIMCOYG <kjhkjh9753@naver.com>
2020-01-23 05:33:10 +0900

    4.2 webpack setting

--- db.js
+++ db.js
@@ -1,17 +1,3 @@
-// export const videos = [
-//     {
-//         id:324393,
-//         title: 'Video awesome',
-//         description: 'This is something I love',
-//         views:24,
-//         videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-//         creator: {
-//             id: 121212,
-//             name: "Nicolas",
-//             email: "nico@las.com",
-//         }
-//     },
-// ];
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path"); //import path from "path"와 같음
+const autoprefixer = require("autoprefixer");
+const ExtractCSS = require("extract-text-webpack-plugin");
+
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js"); //(현재 프로젝트 디렉토리 이름)
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+    entry: ENTRY_FILE,
+    mode: MODE,
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: ExtractCSS.extract([//webpack은 아래에서 위로 실행
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            plugin() {
+                                return [autoprefixer({browsers: "cover 99.5%"})];
+                            }
+                        }
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ])
+            }
+        ]
+    },
+    output: {
+        path: OUTPUT_DIR,
+        filename: "[name].js"
+    },
+    plugins: [new ExtractCSS("styles.css")]
+};
+
+module.exports = config;
