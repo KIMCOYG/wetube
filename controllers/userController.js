37984546dd5ab22732ac4de781e5f06fe0071629
KIMCOYG <kjhkjh9753@naver.com>
2020-01-25 03:06:53 +0900

    7.0 user profile update

--- controllers/userController.js
+++ controllers/userController.js
@@ -116,5 +116,5 @@ export const userDetail = async(req, res) => {
     }
 }
 // export const userDetail = (req, res) => res.render("userDetail", {pageTitle: "User Detail"});
-export const editProfile = (req, res) => res.render("editProfile", {pageTitle: "Edit Profile"});
+export const getEditProfile = (req, res) => res.render("editProfile", {pageTitle: "Edit Profile"});
 export const changePassword = (req, res) =>res.render("changePassword", {pageTitle: "Change Password"});
