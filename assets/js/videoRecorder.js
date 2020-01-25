b5843a3900d2fd3a905da25ccd5069b645891de9
KIMCOYG <kjhkjh9753@naver.com>
2020-01-26 02:52:53 +0900

    9.1 Recording Video Part 1

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,7 +2,21 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = async() => {
+let streamObject;
+
+const handleVideoData = (event) => {
+    console.log(event);
+}
+
+const startRecording = () => {
+    // console.log(streamObject);
+    const videoRecorder = new MediaRecorder(streamObject);
+    videoRecorder.start();
+    // console.log(videoRecorder);
+    videoRecorder.addEventListener("dataavailable", handleVideoData);
+}
+
+const getVideo = async() => {
     try{
         const stream = await navigator.mediaDevices.getUserMedia({
             audio: true,
@@ -12,15 +26,19 @@ const startRecording = async() => {
         videoPreview.srcObject = stream;
         videoPreview.muted = true;
         videoPreview.play();
-        
+        recordBtn.innerHTML = "Stop recording"
+        streamObject = stream;
+        startRecording();
     } catch(error){
         recordBtn.innerHTML = "Can't record";
-        recordBtn.removeEventListener("click", startRecording);
+    } finally{
+        recordBtn.removeEventListener("click", getVideo);
     }
 }
 
 function init(){
-    recordBtn.addEventListener("click", startRecording);
+    recordBtn.addEventListener("click", getVideo);
+    // recordBtn.onclick = getVideo; //오직 하나의 이벤트만 가능, = null은 모든 걸 상쇄시킴
 }
 
 if(recorderContainer){
