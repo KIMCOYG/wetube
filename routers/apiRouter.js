dfcc9732ae4de318cc66ce84310cf1700e9e01a0
KIMCOYG <kjhkjh9753@naver.com>
2020-01-26 18:18:37 +0900

    10.1 API Registering a View Part two

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -2,11 +2,9 @@ import express from "express"
 import routes from "../routes"
 import { registerView } from "../controllers/videoController";
 
-const userRouter = express.Router();
-
 const apiRouter = express.Router();
 
-apiRouter.get(routes.registerView, registerView);
+apiRouter.post(routes.registerView, registerView);
 
 export default apiRouter;
 
